6c361ac70e8f612753c1c62a6f2239f9c0d55cf7
Nattapat-sa <studypeet@gmail.com>
2026-08-04 16:30:42 +0700

    2513310090 submit LabSheet06

---
+++ LabSheets6/src/QueueTest.java
@@ -0,0 +1,20 @@
+
+public class QueueTest {
+
+	public static void main(String[] args) {
+		//QueueArrayBased queue = new QueueArrayBased();
+		
+		QueueLinkedList queue = new QueueLinkedList();
+		queue.enqueue(10);
+		queue.enqueue(11);
+		
+		
+		System.out.println(queue.peek());
+		System.out.println(queue.dequeue());
+		System.out.println(queue.peek());
+		System.out.println(queue.dequeue());
+		System.out.print(queue.peek());
+
+	}
+
+}
